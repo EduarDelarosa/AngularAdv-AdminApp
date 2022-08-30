f063368424391104a3871aaeb0a36c832541cbff
Eduard <eduardelarosa09@gmail.com>
2022-08-30 18:48:56 -0500

    url prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // TODO: Cambiar url de servicios en prod
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://adminapp-one.herokuapp.com/api'
 };
